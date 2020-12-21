efb272c7964dccee305c98e3f7f39e7682d33ec3
GladkaV <vktr.gldk10.98@gmail.com>
2020-12-21 17:53:03 +0200

    changed function uniq (optimized)

--- 03-objects-arrays-intro-to-testing/4-uniq/index.js
+++ 03-objects-arrays-intro-to-testing/4-uniq/index.js
@@ -3,12 +3,6 @@
  * @param {*[]} arr - the array of primitive values
  * @returns {*[]} - the new array with uniq values
  */
-export function uniq(arr) {
-    let result = [];
-
-    if (arr) {
-        result = Array.from(new Set([...arr]));
-    }
-
-    return result;
+export function uniq(arr = []) {
+    return Array.from(new Set([...arr]));
 }
