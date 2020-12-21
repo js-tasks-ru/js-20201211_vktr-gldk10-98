f7303a0366b631a885e0d34f4937654b6979113d
GladkaV <vktr.gldk10.98@gmail.com>
2020-12-21 17:57:51 +0200

    changed function invertObj (optimized)

--- 03-objects-arrays-intro-to-testing/2-invert-object/index.js
+++ 03-objects-arrays-intro-to-testing/2-invert-object/index.js
@@ -4,15 +4,15 @@
  * @returns {object | undefined} - returns new object or undefined if nothing did't pass
  */
 export function invertObj(obj) {
+    let result;
+
     if (obj) {
-        const result = {}
+        result = {}
 
         for (const [key, value] of Object.entries(obj)) {
             result[value] = key;
         }
-
-        return result;
     }
 
-    return;
+    return result;
 }
