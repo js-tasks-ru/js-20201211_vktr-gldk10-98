95b0b6c575455c9da40465e64af367a2196e1c53
GladkaV <vktr.gldk10.98@gmail.com>
2020-12-22 15:02:08 +0200

    added function createGetter

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,5 +4,21 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
+    let pathObj = path.split('.');
 
+    return function (obj) {
+        let object = obj;
+
+        for (let i = 0; i < pathObj.length; i++) {
+
+            if (!object[pathObj[i]]) {
+                object = undefined;
+                break;
+            }
+
+            object = object[pathObj[i]];
+        }
+
+        return object;
+    }
 }
