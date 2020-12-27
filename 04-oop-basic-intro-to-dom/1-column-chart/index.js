616057c4e2ed607e8b2eae5ad7d5f9b0d8af0346
GladkaV <vktr.gldk10.98@gmail.com>
2020-12-27 14:36:05 +0200

    added class ColumnChart

--- 04-oop-basic-intro-to-dom/1-column-chart/index.js
+++ 04-oop-basic-intro-to-dom/1-column-chart/index.js
@@ -1,47 +1,92 @@
 export default class ColumnChart {
-    element = document.createElement('div');
+    subElements = {};
+    chartHeight = 50;
 
-    constructor({ data, label, value, link } = options) {
+    constructor({
+        data = [],
+        label = '',
+        link = '',
+        value = 0
+    } = {}) {
         this.data = data;
         this.label = label;
-        this.value = value;
         this.link = link;
-        this.element.className = "column-chart";
-        this.element.innerHTML = `<div class="column-chart__title">
-                                        Total ${this.label}
-                                        ${this.hasLink(this.link)}
-                                    </div>
-                                    <div class="column-chart__container">
-                                        <div data-element="header" class="column-chart__header" style="--chart-height: 50"> ${this.value} </div>
-                                        <div data-element="body" class="column-chart__chart">
-                                            ${this.tooltip(this.data)}
-                                        </div>
-                                    </div>`
-    };
+        this.value = value;
 
-    tooltip(arr) {
-        if (arr.length === 0) {
-            this.element.classList.add("column-chart_loading");
-        }
-
-        let block = '';
-        let chartHeight = 50;
-
-        arr.forEach(el => {
-            let elHeight = (Math.trunc((chartHeight * el) / 100))
-            block += `<div style="--value: ${elHeight}" data-tooltip="${el}%"></div>`;
-        });
-
-        return block;
-    };
-
-    hasLink(link) {
-        if (link) {
-            return `<a class="column-chart__link" href="${link}">View all</a>`
-        }
+        this.render();
     }
 
-    update() {
-        return [10, 40, 90, 50, 20];
-    }  
+    getColumnBody(data) {
+        const maxValue = Math.max(...data);
+        const scale = this.chartHeight / maxValue;
+
+        return data
+            .map(item => {
+                const percent = (item / maxValue * 100).toFixed(0);
+
+                return `<div style="--value: ${Math.floor(item * scale)}" data-tooltip="${percent}%"></div>`;
+            })
+            .join('');
+    }
+
+    getLink() {
+        return this.link ? `<a class="column-chart__link" href="${this.link}">View all</a>` : '';
+    }
+
+    get template() {
+        return `
+      <div class="column-chart column-chart_loading" style="--chart-height: ${this.chartHeight}">
+        <div class="column-chart__title">
+          Total ${this.label}
+          ${this.getLink()}
+        </div>
+        <div class="column-chart__container">
+           <div data-element="header" class="column-chart__header">
+             ${this.value}
+           </div>
+          <div data-element="body" class="column-chart__chart">
+            ${this.getColumnBody(this.data)}
+          </div>
+        </div>
+      </div>
+    `;
+    }
+
+    render() {
+        const element = document.createElement('div');
+
+        element.innerHTML = this.template;
+
+        this.element = element.firstElementChild;
+
+        if (this.data.length) {
+            this.element.classList.remove('column-chart_loading');
+        }
+
+        this.subElements = this.getSubElements(this.element);
+    }
+
+    getSubElements(element) {
+        const elements = element.querySelectorAll('[data-element]');
+
+        return [...elements].reduce((accum, subElement) => {
+            accum[subElement.dataset.element] = subElement;
+
+            return accum;
+        }, {});
+    }
+
+    update(data) {
+        this.subElements.body.innerHTML = this.getColumnBody(data);
+    }
+
+    remove() {
+        this.element.remove();
+    }
+
+    destroy() {
+        this.remove();
+        this.element = null;
+        this.subElements = {};
+    }
 }
